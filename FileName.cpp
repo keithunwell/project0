8d66b57e0ff55615f0825247123246c3949a40fa
csie <csie@DESKTOP-51E36R8>
2023-12-20 10:59:04 +0900

    添加项目文件。

---
+++ FileName.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int x = 10, y = 10;
+    printf("x=%d\n", x);
+    printf("++x is=%d\n", ++x);
+    printf("x=%d\n\n", x);
+
+    printf("y=%d\n", y);
+    printf("y++ is=%d\n", y++); //计算机看到y就会先执行y
+    printf("y=%d\n", y);
+
+    return 0;
+}
